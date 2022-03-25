d2b81aad10c399da2ea871c82c03ef382c92f2f7
jjl <james.laver@gmail.com>
2022-03-25 17:42:29 +0100

    fix

--- lib/acts/begin.ex
+++ lib/acts/begin.ex
@@ -15,21 +15,21 @@ defmodule Bonfire.Ecto.Acts.Begin do
     # if for some reason something is unlikely to work, we won't bother with the transaction.
     cond do
       epic.errors != [] ->
-        Act.smart(epic, act, epic.errors, "not entering transaction because of errors")
+        smart(epic, act, epic.errors, "not entering transaction because of errors")
         epic = Epic.run(nested)
         %{ epic | next: rest }
       true ->
-        maybe_debug(epic, act, "entering transaction")
+        debug(epic, act, "entering transaction")
         Bonfire.Repo.transact_with(fn ->
           epic = Epic.run(nested)
           if epic.errors == [], do: {:ok, epic}, else: {:error, epic}
         end)
         |> case do
           {:ok, epic} ->
-            maybe_debug(epic, act, "committed successfully.")
+            debug(epic, act, "committed successfully.")
             %{ epic | next: rest }
           {:error, epic} ->
-            maybe_debug(epic, act, "rollback because of errors")
+            debug(epic, act, "rollback because of errors")
             %{ epic | next: rest }
         end
     end
