d35a0be22242eb0d6b32159e2bdd0c13ba705f1a
Mayel de Borniol <git@mayel.space>
2024-04-12 13:38:35 +0100

    module

--- lib/acts/commit.ex
+++ lib/acts/commit.ex
@@ -1,18 +1,16 @@
-if Code.ensure_loaded?(Bonfire.Common.Utils) do
-  defmodule Bonfire.Ecto.Acts.Commit do
-    @moduledoc """
-    A placeholder marker used by Begin to identify when to commit the transaction.
-    """
+defmodule Bonfire.Ecto.Acts.Commit do
+  @moduledoc """
+  A placeholder marker used by Begin to identify when to commit the transaction.
+  """
 
-    def run(epic, act) do
-      raise RuntimeError,
-        message: """
-        Bonfire.Ecto: Attempted to Commit without a Begin first!
+  def run(epic, act) do
+    raise RuntimeError,
+      message: """
+      Bonfire.Ecto: Attempted to Commit without a Begin first!
 
-        epic: #{inspect(epic)}
+      epic: #{inspect(epic)}
 
-        act: #{inspect(act)}
-        """
-    end
+      act: #{inspect(act)}
+      """
   end
 end
