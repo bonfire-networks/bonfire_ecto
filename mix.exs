f41f531b735e3186b752d9aef12145499c8e577b
Mayel de Borniol <git@mayel.space>
2024-04-14 20:00:04 +0100

    deps

--- mix.exs
+++ mix.exs
@@ -33,10 +33,9 @@ defmodule Bonfire.Ecto.MixProject do
       # {:bonfire_epics, "~> 0.1"},
       {:bonfire_common,
        git: "https://github.com/bonfire-networks/bonfire_common",
-       branch: "main",
        optional: true},
       {:bonfire_epics,
-       git: "https://github.com/bonfire-networks/bonfire_epics", branch: "main"}
+       git: "https://github.com/bonfire-networks/bonfire_epics"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
